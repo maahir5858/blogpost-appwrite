4aba4524b8a141806a2ac5817cb5b7e2533a27e0
Maahir <maahirchoudhary1@gmail.com>
2026-06-23 01:33:22 +0530

    create all the Pages

--- src/components/index.js
+++ src/components/index.js
@@ -3,8 +3,14 @@ import Footer from "./Footer/Footer";
 import Container from './container/Container'
 import Logo from "./Logo";
 import LogoutBtn from './Header/LogoutBtn'
+import Select from './Select'
+import Input from './Input'
+import Button from './Button'
 import PostCard from './PostCard'
+import Login from './Forms/Login'
+import Signup from './Forms/Signup'
 import RTE from "./post-form/RTE";
 import PostForm from "./post-form/PostForm";
+import AuthLayout from './AuthLayout'
 
-export { Header, Footer, Container, Logo, LogoutBtn, PostCard, RTE, PostForm }
+export { Header, Footer, Container, Logo, LogoutBtn, Select, Input, Button, PostCard, Login, Signup, RTE, PostForm, AuthLayout }
---
+++ src/pages/EditPost.jsx
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from 'react'
+import { Container, PostForm } from '../components'
+import postService from '../appwrite/postService'
+import { useNavigate, useParams } from 'react-router-dom';
+
+function EditPost() {
+    const [post, setPost] = useState(null);
+    const {slug} = useParams();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (slug) {
+            postService.getPost(slug)
+                .then((post) => setPost(post));
+        } else {
+            navigate("/");
+        }
+    }, [navigate, slug]);
+    
+    return post ? (
+        <div className='py-8'>
+            <Container>
+                <PostForm post={post} />
+            </Container>
+        </div>
+    ) : null;
+}
+
+export default EditPost
